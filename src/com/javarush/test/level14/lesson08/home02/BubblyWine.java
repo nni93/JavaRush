3ddccc5bbb160f48ec1638502964f447f1af7fd5
Natalia Nikolaieva <nni93@inbox.ru>
2013-09-06 21:30:33 +0200

    Update BubblyWine.java

--- src/com/javarush/test/level14/lesson08/home02/BubblyWine.java
+++ src/com/javarush/test/level14/lesson08/home02/BubblyWine.java
@@ -2,6 +2,6 @@ package com.javarush.test.level14.lesson08.home02;
 
 class BubblyWine extends Wine {
     public String getHolidayName() {
-        return "Новый год";
+        return "РќРѕРІС‹Р№ РіРѕРґ";
     }
 }
