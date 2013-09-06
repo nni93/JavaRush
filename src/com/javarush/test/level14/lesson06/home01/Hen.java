c317ada1ebab887eb6c2b3dd3e24477712e88227
Natalia Nikolaieva <nni93@inbox.ru>
2013-09-06 21:25:57 +0200

    Update Hen.java

--- src/com/javarush/test/level14/lesson06/home01/Hen.java
+++ src/com/javarush/test/level14/lesson06/home01/Hen.java
@@ -3,6 +3,6 @@ package com.javarush.test.level14.lesson06.home01;
 abstract class Hen {
     public abstract int getCountOfEggsPerMonth();
     String getDescription() {
-       return "Я курица.";
+       return "РЇ РєСѓСЂРёС†Р°.";
     }
 }
